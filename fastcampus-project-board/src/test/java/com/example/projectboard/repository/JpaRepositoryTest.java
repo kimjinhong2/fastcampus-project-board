b2101ecb41e8b9a4ecac608e8cee55ed20f84d31
kimjinhong2 <qkrgk0625@naver.com>
2023-10-18 14:48:43 +0900

    #12 - jpa test 작성
    기본 crud + 연관관계 매핑과 cascading이 작 동작하는지 보려고 작성.
    테스트 데이터와 관련이 있기 때문에, 이대로는 실패한다

---
+++ fastcampus-project-board/src/test/java/com/example/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.example.projectboard.repository;
+
+import com.example.projectboard.config.JpaConfig;
+import com.example.projectboard.domain.Article;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+        //given
+
+
+        //when
+        List<Article> articles=articleRepository.findAll();
+
+
+        //then
+        Assertions.assertThat(articles).isNotNull().hasSize(1123);
+    }
+}
